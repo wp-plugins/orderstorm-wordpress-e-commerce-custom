fdc01a5193c22ec87eed5723f66cbe55e11b62e9
carlosman <carlosman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-29 16:23:04 +0000

    = 0.5.0.2 [2011.12.29] = * Added CSS for product list pagination in the Product Category page
    git-svn-id: https://plugins.svn.wordpress.org/orderstorm-wordpress-e-commerce-custom/tags/0.5.0.2-2011.12.29@481920 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ orderstorm-ecommerce-custom.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: OrderStorm e-Commerce Custom Files Manager
+Plugin URI: http://www.orderstorm.com/wordpress-ecommerce
+Description: A plugin to manage .css and other files for the OrderStorm WordPress e-Commerce Plugin
+Version: 0.5.0.2
+Author: OrderStorm, Inc.
+Author URI: http://www.orderstorm.com
+License: GPL2 or later
+*/
+
+/*
+	Copyright (C) 2010-2011 OrderStorm, Inc. (e-mail: wordpress-ecommerce@orderstorm.com)
+	
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
